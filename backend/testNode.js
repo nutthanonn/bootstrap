ba54e64d9bd747ef024f1859344cd09d3a94f472
nutthanon <nutthanon.tho@gmail.com>
2021-07-28 14:57:59 +0700

    [+] beta

--- backend/testNode.js
+++ backend/testNode.js
@@ -1,12 +1,1 @@
-const greeting = (name) => {
-    console.log(`Hello ${name}`);
-}
-
-
-const  result =  (fname, lname, callback) => {
-    const fullName = `${fname} ${lname}`
-
-    callback(fullName)
-}
-
-result('Nutthanon', 'Thongcharoen', greeting)
+console.log('hello')
