f2ca10e51cfac69b991cc22a5dcc843c092d546a
nutthanon <nutthanon.tho@gmail.com>
2021-08-10 15:24:20 +0700

    [+] Project

--- project/Portfolio/script.js
+++ project/Portfolio/script.js
@@ -1,19 +1,32 @@
-var i = 1
+let i = 2
+let count = 0;
+let items = document.getElementById(`img1`)
+let textapple = document.getElementById(`text-apple-1`)
+textapple.style.display = 'block'
+items.style.display = 'block'
 
 setInterval(()=>{
-    let imgShow = document.getElementById(`img${i}`)
-
+    count++
+    console.log(count);
     if(i == 1){
-        let lastpic = document.getElementById(`img4`)
-        lastpic.classList.remove('show')
+        document.getElementById('img3').style.display = 'none'
+        document.getElementById('text-apple-3').style.display = 'none'
+
     }else{
-        let lastpic = document.getElementById(`img${i-1}`)
-        lastpic.classList.remove('show')
+        document.getElementById(`img${i-1}`).style.display = 'none'
+        document.getElementById(`text-apple-${i-1}`).style.display = 'none'
     }
+
+
+    const items = document.getElementById(`img${i}`)
+    const textapple = document.getElementById(`text-apple-${i}`)
+    items.style.display = 'block'
+    textapple.style.display = 'block'
+
 
     i++
-    imgShow.classList.add('show')
-    if(i > 4){
+    if(i > 3){
         i = 1
     }
-}, 3500)
+
+}, 5000)
