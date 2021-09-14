6c0681c02e9e9be8208483ff374d92942a822104
nutthanon <nutthanon.tho@gmail.com>
2021-09-15 00:02:16 +0700

    [+] Project

--- Project/Lazyload/script.js
+++ Project/Lazyload/script.js
@@ -1,18 +1,16 @@
-const container = document.getElementById('square')
-const obserevr = new IntersectionObserver(onIntersection)
+const container = document.getElementById("square");
+const obserevr = new IntersectionObserver(onIntersection);
 
-obserevr.observe(container)
+obserevr.observe(container);
 
-function onIntersection(entry){
-  let element = entry[0]
-  
+function onIntersection(entry) {
+  let element = entry[0];
 
-  if(element.intersectionRatio > 0){
-    container.classList.add('hello')
+  if (element.intersectionRatio > 0) {
+    container.classList.add("hello");
 
-    observer.unobserve(element.target)
-    observer.disconnect()
+    observer.unobserve(element.target);
+    observer.disconnect();
   }
-  container.classList.remove('hello')
+  container.classList.remove("hello");
 }
-
--- Project/server.js
+++
@@ -1,5 +0,0 @@
-a = [1, 3, 44, 5];
-a.map((e, index) => {
-  let a = index % 2 ? "even" : "odd";
-  console.log(a, e);
-});
