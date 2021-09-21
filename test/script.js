c0bb2c3101ab8a272ddfad74cf1650c7d1291fb9
nutthanon <nutthanon.tho@gmail.com>
2021-09-21 18:51:13 +0700

    [+] Project

--- test/script.js
+++ test/script.js
@@ -1,15 +1,17 @@
 //Regular Expression
-const name = [
-  "Nutthanon Thongcharoen",
-  "JavaScript",
-  "Python",
-  "ReactJS",
-  "HTML",
-  "Macbook Pro",
-];
+let re = new RegExp(
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+);
+
 const email = [
   "nutthanon.tho@gmail.com",
   "JavaScript@mail",
   "Python@com",
-  "ReactJS@gmail.nnn",
+  "ReactJS@gmail.hello",
 ];
+
+email.forEach((e) => {
+  return console.log(re.test(e));
+});
+
+// console.log(re.test("nutthanon.tho@gmail.com"));
