93846bf0ea3a64b8d77eaa6007f64a221955fb45
nutthanon <nutthanon.tho@gmail.com>
2021-09-21 18:29:26 +0700

    [+] Project

---
+++ test/script.js
@@ -0,0 +1,15 @@
+//Regular Expression
+const name = [
+  "Nutthanon Thongcharoen",
+  "JavaScript",
+  "Python",
+  "ReactJS",
+  "HTML",
+  "Macbook Pro",
+];
+const email = [
+  "nutthanon.tho@gmail.com",
+  "JavaScript@mail",
+  "Python@com",
+  "ReactJS@gmail.nnn",
+];
